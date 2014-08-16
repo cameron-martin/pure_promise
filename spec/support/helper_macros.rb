ca08cf9dac5674227241f56c93f09482fb31a881
Cameron Martin <cameronmartin123@gmail.com>
2014-08-16 03:21:47 +0100

    Made state expectations more DRY.

--- spec/support/helper_macros.rb
+++ spec/support/helper_macros.rb
@@ -1,44 +1,37 @@
 module HelperMacros
-
-  # TODO: Dry this up
-
   def expect_fulfillment(promise, options={})
-    fulfill_callback, reject_callback = create_callback_doubles
+    setup_state_expectation(promise) do |fulfill_callback, reject_callback|
+      yield if block_given?
 
-    promise.then(fulfill_callback, reject_callback)
-
-    yield if block_given?
-
-    expect(fulfill_callback).to have_received(:call).with(options[:with] || anything)
-    expect(reject_callback).to_not have_received(:call)
+      expect(fulfill_callback).to have_received(:call).with(options[:with] || anything)
+      expect(reject_callback).to_not have_received(:call)
+    end
   end
 
   def expect_rejection(promise, options={})
-    fulfill_callback, reject_callback = create_callback_doubles
+    setup_state_expectation(promise) do |fulfill_callback, reject_callback|
+      yield if block_given?
 
-    promise.then(fulfill_callback, reject_callback)
-
-    yield if block_given?
-
-    expect(reject_callback).to have_received(:call).with(options[:with] || anything)
-    expect(fulfill_callback).to_not have_received(:call)
+      expect(reject_callback).to have_received(:call).with(options[:with] || anything)
+      expect(fulfill_callback).to_not have_received(:call)
+    end
   end
 
   def expect_pending(promise)
-    fulfill_callback, reject_callback = create_callback_doubles
-
-    promise.then(fulfill_callback, reject_callback)
-
-    expect(fulfill_callback).to_not have_received(:call)
-    expect(reject_callback).to_not have_received(:call)
+    setup_state_expectation(promise) do |fulfill_callback, reject_callback|
+      expect(fulfill_callback).to_not have_received(:call)
+      expect(reject_callback).to_not have_received(:call)
+    end
   end
 
 private
 
-  def create_callback_doubles
-    [
-      double('fulfill_callback', call: PurePromise.fulfill),
-      double('reject_callback', call: PurePromise.reject)
-    ]
+  def setup_state_expectation(promise)
+    fulfill_callback = double('fulfill_callback', call: PurePromise.fulfill)
+    reject_callback = double('reject_callback', call: PurePromise.reject)
+
+    promise.then(fulfill_callback, reject_callback)
+
+    yield fulfill_callback, reject_callback
   end
 end
